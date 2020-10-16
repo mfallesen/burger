01d18d8c50062c4fc7d1bd0929d992d063830e45
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-15 23:24:04 -0700

    update burger model

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO burgers (name) VALUES ('Big Mac');
-INSERT INTO burgers (name) VALUES ('Whopper');
-INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Five Guys Cheeseburger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Royal Red Robin ', true);
-INSERT INTO burgers (name) VALUES ('Guacamole Bacon');
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Five Guys Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royal Red Robin ', true);
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Bacon');
